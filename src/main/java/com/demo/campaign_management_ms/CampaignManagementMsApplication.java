51e8a001b1d8b1a85b0410e0302f8253fcd532c6
manishk9620 <manish_kumar@persistent.com>
2021-08-10 12:12:25 +0530

    Added swagger Impl
    Swagger added

--- src/main/java/com/demo/campaign_management_ms/CampaignManagementMsApplication.java
+++ src/main/java/com/demo/campaign_management_ms/CampaignManagementMsApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.demo"} )
-@EnableMongoRepositories("com.demo.campaign_management_ms_repo")
+@EnableMongoRepositories("com.demo.campaign_management_ms.repo")
+@EnableSwagger2
 public class CampaignManagementMsApplication {
 
 	public static void main(String[] args) {
